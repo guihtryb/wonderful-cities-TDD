98a3b4613644ff9526d46c035ef5522a2838b562
Guilherme Viana <guilhermehviana01@gmail.com>
2022-10-10 17:17:37 -0300

    feat: add smooth scrolling

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 import './MenuItem.css';
 
 export default function MenuItem({ itemName }) {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const href = event.target.getAttribute('href');
+
+    const goTo = document.querySelector(href);
+
+    goTo.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <li className="menu-item">
-      <a href={`#${itemName}`} data-testid={`${itemName}-item`}>
+      <a onClick={handleClick} href={`#${itemName}`} data-testid={`${itemName}-item`}>
         {itemName}
       </a>
     </li>
