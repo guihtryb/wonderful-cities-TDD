d5b66ce8ccb8362206e39ae3b869e7b2a771defe
Guilherme Viana <guilhermehviana01@gmail.com>
2022-10-11 17:14:59 -0300

    test: add cypress e2e initial tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,31 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
+
+describe('Testing home page',() => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('GET', '**/cities', 'fixture:cities');
+    cy.visit('/');
+  });
+
+  it('home page previous slide works correctly', () => {
+    cy.get('#slide-button-previous').click();
+    cy.get('[data-testid="city-name"]').contains(/Curitiba/i)
+    cy.get('#slide-button-previous').click();
+    cy.get('[data-testid="city-name"]').contains(/Singapore/i)
+  });
+
+  it('home page next slide works correctly', () => {
+    cy.get('#slide-button-next').click();
+    cy.get('[data-testid="city-name"]').contains(/Curitiba/i)
+    cy.get('#slide-button-next').click();
+    cy.get('[data-testid="city-name"]').contains(/Singapore/i)
+  });
+
+  it('home page FAQ accordion works correctly', () => {
+    cy.get('dt').should((dt) => {
+      expect(dt).to.have.length(3)
+    });
+    cy.get('dd').should('have.css', 'display', 'none');
+    cy.get('dt').first().click();
+    cy.get('dd').first().should('have.css', 'display', 'block');
   });
 });
